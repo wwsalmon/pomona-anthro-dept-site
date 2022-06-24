29d6ca5a1e83b2c60f825e24dd4dca856423a1f6
Samson Zhang <wwsamson@yahoo.com>
2022-06-24 17:01:15 -0400

    add basic styling

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php get_header();
 ?>
-<h1>
-    Student ethnographies @ Pomona
-</h1>
+    <div id="top-container">
+        <img src="<?php echo get_template_directory_uri()?>/assets/pomona.jpg" alt="Pomona College logo" style="width: 100px">
+        <h1 style="font-size: 36px">
+            Student ethnographies @ Pomona
+        </h1>
+        <p style="margin: 16px 0; font-size: 20px">
+            Archives of student work from the anthropology department at Pomona College
+        </p>
+    </div>
 <?php
 get_footer();
