6ac944c9d51d3b630d202de595f09d2cfd8bc187
Samson Zhang <wwsamson@yahoo.com>
2022-06-24 23:20:32 -0400

    basic items display

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
             Archives of student work from the anthropology department at Pomona College
         </p>
     </div>
-    <div class="item-grid">
+    <div id="item-grid">
 		<?php
 		$args  = array(
 			'post_type' => 'Student work',
@@ -19,8 +19,34 @@
 		$items = new WP_Query( $args );
 		if ( $items->have_posts() ) :
 			while ( $items->have_posts() ) : $items->the_post();
+                $cats = get_the_category();
+                $is_embed = has_category('StoryMap');
 				?>
-                <h2><?php the_title(); ?></h2>
+                <a class="item" href="<?php echo get_post_meta(get_the_ID(), 'url', TRUE); ?>">
+                    <div class="item-top-bar">
+                        <?php
+                            foreach ($cats as $cat) {
+                                ?>
+                                    <button class="item-category"><span><?php echo $cat->name?></span></button>
+                                <?php
+                            }
+                        ?>
+                    </div>
+                    <?php
+                    if ($is_embed) {
+                        ?>
+                        <iframe src="<?php echo get_post_meta(get_the_ID(), 'url', TRUE); ?>" class="item-embed"></iframe>
+                        <?php
+                    } else {
+	                    ?>
+                        <h2><?php the_title(); ?></h2>
+	                    <?php
+                    }
+                    ?>
+                    <div class="item-bottom-bar">
+                        <?php echo get_post_meta(get_the_ID(), 'author', TRUE); ?>
+                    </div>
+                </a>
 			<?php
 			endwhile;
 			wp_reset_postdata();
